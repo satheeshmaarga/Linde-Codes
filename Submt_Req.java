a01d2a49ebc8077b2ef73652380d38776a6d3037
sam <satheeshv.ece@gmail.com>
2015-05-08 17:25:38 +0530

    Code Chages

--- Submt_Req.java
+++ Submt_Req.java
@@ -58,6 +58,7 @@ public class Submt_Req {
   	  //driver1.findElement(By.partialLinkText(text)).click();
 	  System.out.println("1st updated code section to check whether commited")
 System.out.println("1st updated code section to check whether commited")
+System.out.println("1st updated code section to check whether commited")
   	  }
 }
   
